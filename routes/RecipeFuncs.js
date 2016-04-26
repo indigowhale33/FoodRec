d69e4e3a90b917f36be28118cf46610e7ce36037
ttm8 <thotran.public@gmail.com>
2016-04-26 07:58:00 -0400

    Update nutrition facts

--- routes/RecipeFuncs.js
+++ routes/RecipeFuncs.js
@@ -33,7 +33,7 @@ var RecipeFuncs = function() {
 
         var queryDB = require('../database')(queryText, function(mssg, data) {
             
-            console.log(JSON.stringify(data));
+            //console.log(JSON.stringify(data));
 
             if(data == null || data.length < 1) {
                 sendMessage(res, 400, "Could not find any recipe matching that ID", data, "Get recipe by ID");
@@ -75,6 +75,22 @@ var RecipeFuncs = function() {
     */
     var generatePossibleRecipesFromPantry = function(userName, res) {
 
+        // generate all recipes which match exactly the contents in pantry
+        /*
+        var queryText = "SELECT * " +
+                        "FROM " +
+                        "( " +
+                            "SELECT recipe_id " +
+                            "FROM recipe_ingredients ri LEFT JOIN pantries p " +
+                                "ON ri.ingredient_id = p.ingredient_id AND p.owner_name = \'" + userName + "\' " +
+                            "GROUP BY recipe_id " +
+                            "HAVING (COUNT(*) > 1) AND ((COUNT(*) = COUNT(p.ingredient_id)) " +
+                                    "OR (COUNT(*)  - 1 = COUNT(p.ingredient_id)) " +
+                                    "OR (COUNT(*)  + 1 = COUNT(p.ingredient_id))) " +
+                        ") q JOIN recipes r " +
+                            "ON q.recipe_id = r.id";
+        */
+
         var queryText = "SELECT * " +
                         "FROM " +
                         "( " +
@@ -86,6 +102,7 @@ var RecipeFuncs = function() {
                         ") q JOIN recipes r " +
                             "ON q.recipe_id = r.id";
 
+
         var queryDB = require('../database')(queryText, function(mssg, data) {
             
             if(data == null || data.length == 0 || data[0]['recipe_id'] == null) {
@@ -93,7 +110,7 @@ var RecipeFuncs = function() {
                                     "Could not find any recipes (recipe_id does not exist)");
            }
            else {
-                console.log("data: " + JSON.stringify(data, null, 2));
+                //console.log("data: " + JSON.stringify(data, null, 2));
                 sendMessage(res, 200, "Success!", data.splice(0,100), "Get recipe based on ingredients in pantry");
            }          
         }); 
@@ -117,15 +134,13 @@ var RecipeFuncs = function() {
                 vitk: 0.00, cholesterol: 0.00
             }; 
 
-        var units = ['cup', 'teaspoon', 'tablespoon', 'inch', 
-                    'ounce', 'oz', 'pound', 'lb', 'pint', 'gram', 'g'];
-
         var queryText = squel.select()
-                                .from("recipe_ingredients as ri")
-                                .from("ingredients as i")
-                                .where("ri.recipe_id = " + recipeID)
-                                .where("ingredient_id = i.id")
-                                .toString();
+                            .from("recipes, recipe_ingredients as ri")
+                            .from("ingredients as i")
+                            .where("ri.recipe_id = " + recipeID)
+                            .where("ingredient_id = i.id")
+                            .where("recipes.id = ri.recipe_id")
+                            .toString();
 
         var queryDB = require('../database')(queryText, function(mssg, data) {                 
             
@@ -134,6 +149,7 @@ var RecipeFuncs = function() {
                 return;
             }
 
+            //console.log("data: " + JSON.stringify(data));
             sumNutritionInfo(res, mssg, data);
         });
 
@@ -142,121 +158,69 @@ var RecipeFuncs = function() {
             for(var i = 0; i < data.length; i++) {
 
                 var amountStr = data[i]['amount'];
-                var amountArr = amountStr.split(' ');
 
-                var j;
-                var unitsNdx;
-                for(j = 0; j < amountArr.length; j++) {
-                    unitsNdx = findInArray(units, amountArr[j]);
-                    if(unitsNdx >= 0) {
-                        break;
-                    }
-                }
-
-                if(j >= amountArr.length - 1)
-                    continue;
-
-                var parsed;
-
-                // check if fraction
-                if(findInArray(amountArr[j+1], '/') >= 0) {
-                    parsed = eval(amountArr[j+1]);
-                }
-                // else if not fraction but is a numeric value
-                else if(!isNaN(parseFloat(amountArr[j+1])) && isFinite(amountArr[j+1])) {
-                    parsed = parseFloat(amountArr[j+1]);                        
-                }
-                // else if NaN
-                else {
+                if(isNaN(amountStr) && isFinite(amountStr)) {
                     console.log("NaN!!");
                     continue;
                 }
 
-                var gramVal = convertToGrams(parsed, unitsNdx)/100; /// WHY DIVIDE BY 100???
-                updateTotals(res, totals, gramVal, data, i);
+                var gramVal = parseFloat(amountStr); /// WHY DIVIDE BY 100???
+                var recipeYield = parseFloat(data[i]['recipe_yield']);
+                
+                //console.log(data);
+
+                updateTotals(res, totals, gramVal, recipeYield, data, i);
             }
 
             sendMessage(res, 200, mssg, totals, "Generate nutrition facts for recipe");
         }
 
-        function updateTotals(res, totals, gramVal, data, i) {
+        function updateTotals(res, totals, gramVal, recipeYield, data, i) {
 
             // nutrient content: V = (N * CM)/100; pg 44
             // grams
             totals['serving_size']      += gramVal;
-            totals['kcals']             += (parseFloat(data[i]['kcals']) * gramVal)/100;
-            totals['protein']           += (parseFloat(data[i]['protein']) * gramVal)/100; 
-            totals['fat']               += (parseFloat(data[i]['fat']) * gramVal)/100;
-            totals['carbohydrates']     += (parseFloat(data[i]['carbohydrates']) * gramVal)/100;   
+            //console.log("gramVal: " + gramVal);
+            //console.log("serving: " + recipeYield);
+            //console.log("float: " + parseFloat(data[i]['kcals']));
+
+            var mfloat = parseFloat(data[i]['kcals']);
+            if(mfloat == 0 || gramVal == 0)
+                return; 
+
+            totals['kcals']             += (gramVal / 100) * (parseFloat(data[i]['kcals']));
+            totals['protein']           += (gramVal / 100) * (parseFloat(data[i]['protein']));
+            totals['fat']               += (gramVal / 100) * (parseFloat(data[i]['fat']));
+            totals['carbohydrates']     += (gramVal / 100) * (parseFloat(data[i]['carbohydrates']));   
 
             // daily % value             
-            totals['fiber']             += (parseFloat(data[i]['fiber']) * gramVal);
-            totals['calcium']           += (parseFloat(data[i]['calcium']) * gramVal);
-            totals['iron']              += (parseFloat(data[i]['iron']) * gramVal);
-            totals['magnesium']         += (parseFloat(data[i]['magnesium']) * gramVal);
-            totals['phosphorous']       += (parseFloat(data[i]['phosphorous']) * gramVal);
-            totals['potassium']         += (parseFloat(data[i]['potassium']) * gramVal);
-            totals['sodium']            += (parseFloat(data[i]['sodium']) * gramVal);
-            totals['zinc']              += (parseFloat(data[i]['zinc']) * gramVal);
-            totals['copper']            += (parseFloat(data[i]['copper'])  * gramVal);
-            totals['manganese']         += (parseFloat(data[i]['manganese']) * gramVal);
-            totals['selenium']          += (parseFloat(data[i]['selenium']) * gramVal);
-            totals['vitc']              += (parseFloat(data[i]['vitc']) * gramVal);
-            totals['thiamin']           += (parseFloat(data[i]['thiamin']) * gramVal);
-            totals['riboflavin']        += (parseFloat(data[i]['riboflavin']) * gramVal);
-            totals['niacin']            += (parseFloat(data[i]['niacin']) * gramVal);
-            totals['pantothenic_acid']  += (parseFloat(data[i]['pantothenic_acid']) * gramVal);
-            totals['vitb6']             += (parseFloat(data[i]['vitb6']) * gramVal);
-            totals['folate']            += (parseFloat(data[i]['folate']) * gramVal);
-            totals['choline']           += (parseFloat(data[i]['choline']) * gramVal);
-            totals['vitb12']            += (parseFloat(data[i]['vitb12']) * gramVal);
-            totals['vita']              += (parseFloat(data[i]['vita']) * gramVal);
-            totals['vite']              += (parseFloat(data[i]['vite']) * gramVal);
-            totals['vitd']              += (parseFloat(data[i]['vitd']) * gramVal);
-            totals['vitk']              += (parseFloat(data[i]['vitk']) * gramVal);
-            totals['cholesterol']       += (parseFloat(data[i]['cholesterol']) * gramVal);
-        }
+            totals['fiber']             += (parseFloat(data[i]['fiber']) * (gramVal / 100));
+            totals['calcium']           += (parseFloat(data[i]['calcium']) * (gramVal / 100));
+            totals['iron']              += (parseFloat(data[i]['iron']) * (gramVal / 100));
+            totals['magnesium']         += (parseFloat(data[i]['magnesium']) * (gramVal / 100));
+            totals['phosphorous']       += (parseFloat(data[i]['phosphorous']) * (gramVal / 100));
+            totals['potassium']         += (parseFloat(data[i]['potassium']) * (gramVal / 100));
+            totals['sodium']            += (parseFloat(data[i]['sodium']) * (gramVal / 100));
+            totals['zinc']              += (parseFloat(data[i]['zinc']) * (gramVal / 100));
+            totals['copper']            += (parseFloat(data[i]['copper'])  * (gramVal / 100));
+            totals['manganese']         += (parseFloat(data[i]['manganese']) * (gramVal / 100));
+            //console.log("manganese: " + parseFloat(data[i]['manganese'])/recipeYield * (gramVal / 100));
 
-        function convertToGrams(parsed, unitsNdx) {
-
-            var gramVal;
-
-            // cup
-            if(unitsNdx == 0) {
-                gramVal = parsed * 236.58824;
-            }
-            // teaspoon
-            else if(unitsNdx == 1) {
-                gramVal = parsed * 5;
-            }
-            // tablespoon
-            else if(unitsNdx == 2) {
-                gramVal = parsed * 15; 
-            }
-            // cubic inch
-            else if(unitsNdx == 3) {
-                gramVal = parsed * parsed * parsed * 16.3870640693;
-            }
-            // ounce
-            else if(unitsNdx == 4 || unitsNdx == 5) {
-                gramVal = parsed * 28.3495;
-            }
-            // pound mass
-            else if(unitsNdx == 6 || unitsNdx == 7) {
-                gramVal = parsed * 453.592;
-            }
-            // pint
-            else if(unitsNdx == 8) {
-                gramVal = parsed * 473.1764750005525;
-            }
-
-            // more to add if i find more in recipe_inredients table
-
-            return gramVal;
-        }
-
-        function findInArray(arr, word) {
-            return arr.indexOf(word);
+            totals['selenium']          += (parseFloat(data[i]['selenium']) * (gramVal / 100));
+            totals['vitc']              += (parseFloat(data[i]['vitc']) * (gramVal / 100));
+            totals['thiamin']           += (parseFloat(data[i]['thiamin']) * (gramVal / 100));
+            totals['riboflavin']        += (parseFloat(data[i]['riboflavin']) * (gramVal / 100));
+            totals['niacin']            += (parseFloat(data[i]['niacin']) * (gramVal / 100));
+            totals['pantothenic_acid']  += (parseFloat(data[i]['pantothenic_acid']) * (gramVal / 100));
+            totals['vitb6']             += (parseFloat(data[i]['vitb6']) * (gramVal / 100));
+            totals['folate']            += (parseFloat(data[i]['folate']) * (gramVal / 100));
+            totals['choline']           += (parseFloat(data[i]['choline']) * (gramVal / 100));
+            totals['vitb12']            += (parseFloat(data[i]['vitb12']) * (gramVal / 100));
+            totals['vita']              += (parseFloat(data[i]['vita']) * (gramVal / 100));
+            totals['vite']              += (parseFloat(data[i]['vite']) * (gramVal / 100));
+            totals['vitd']              += (parseFloat(data[i]['vitd']) * (gramVal / 100));
+            totals['vitk']              += (parseFloat(data[i]['vitk']) * (gramVal / 100));
+            totals['cholesterol']       += (parseFloat(data[i]['cholesterol']) * (gramVal / 100));
         }
     };
 
@@ -269,7 +233,7 @@ var RecipeFuncs = function() {
         var queryText_check = "SELECT ri_inner.ingredient_id " +
                             "FROM recipe_ingredients ri_inner JOIN pantries p " +
                             "ON p.owner_name=\'" + user_name + "\' AND ri_inner.ingredient_id = p.ingredient_id " +
-                            ";";
+                            "LIMIT 1;";
 
         var queryDB_check = require('../database')(queryText_check, function(mssg, data) { 
 
@@ -283,64 +247,64 @@ var RecipeFuncs = function() {
             next_step(user_name, res);
         });
 
-        function next_step(user_name, res) {
+    function next_step(user_name, res) {
 
-            var queryText = "SELECT * " +
-                        "FROM recipe_ingredients ri_outer " +
-                        "WHERE ri_outer.ingredient_id NOT IN " +
-                        "( " +
-                            "SELECT ri_inner.ingredient_id " +
-                            "FROM recipe_ingredients ri_inner JOIN pantries p " +
-                            "ON p.owner_name=\'" + user_name + "\' AND ri_inner.ingredient_id = p.ingredient_id " +
-                        ");";
+        var queryText = "SELECT * " +
+                    "FROM recipe_ingredients ri_outer " +
+                    "WHERE ri_outer.ingredient_id NOT IN " +
+                    "( " +
+                        "SELECT ri_inner.ingredient_id " +
+                        "FROM recipe_ingredients ri_inner JOIN pantries p " +
+                        "ON p.owner_name=\'" + user_name + "\' AND ri_inner.ingredient_id = p.ingredient_id " +
+                    ");";
 
-            var queryDB = require('../database')(queryText, function(mssg, data) {                 
+        var queryDB = require('../database')(queryText, function(mssg, data) {                 
 
-                //console.log("data: ")
-                if(data == null || data.length < 1) {
-                    sendMessage(res, 400, "Could not find any ingredients that will help : " + user_name, data, "Get possible recipes");
-                    return;
+            //console.log("data: ")
+            if(data == null || data.length < 1) {
+                sendMessage(res, 400, "Could not find any ingredients that will help : " + user_name, data, "Get possible recipes");
+                return;
+            }
+            
+            var formatted = [];
+            for(var i = 0; i < data.length; i++) {
+
+                var keyExistsInArr = false;
+                for(var j = 0; j < formatted.length; j++) {
+
+                    if(formatted[j] == null)
+                        continue;
+                    
+                    if(formatted[j]['recipe_id'] == data[i]['recipe_id']) {
+                        keyExistsInArr = true;
+                        formatted[j]['ingredients_needed'].push({"ingredient_id":data[i]['ingredient_id'], "amount":data[i]['amount']});
+                        formatted[j]['ingredients_needed_count'] += 1;
+                    }
                 }
-                
-                var formatted = [];
-                for(var i = 0; i < data.length; i++) {
 
-                    var keyExistsInArr = false;
-                    for(var j = 0; j < formatted.length; j++) {
+                if(!keyExistsInArr) {
 
-                        if(formatted[j] == null)
-                            continue;
-                        
-                        if(formatted[j]['recipe_id'] == data[i]['recipe_id']) {
-                            keyExistsInArr = true;
-                            formatted[j]['ingredients_needed'].push({"ingredient_id":data[i]['ingredient_id'], "amount":data[i]['amount']});
-                            formatted[j]['ingredients_needed_count'] += 1;
+                    formatted.push(
+                        {
+                            "recipe_id": data[i]['recipe_id'],
+                            "ingredients_needed_count": 1,
+                            "ingredients_needed": [{"ingredient_id": data[i]['ingredient_id'], 
+                                            "amount":data[i]['amount']
+                            }]
                         }
-                    }
-
-                    if(!keyExistsInArr) {
-
-                        formatted.push(
-                            {
-                                "recipe_id": data[i]['recipe_id'],
-                                "ingredients_needed_count": 1,
-                                "ingredients_needed": [{"ingredient_id": data[i]['ingredient_id'], 
-                                                "amount":data[i]['amount']
-                                }]
-                            }
-                        );
-                    }
+                    );
                 }
+            }
 
-                if(formatted.length == 0) {
-                    sendMessage(res, 400, "Could not find any other ingredients for any other recipes", [], "Get ingredients that, when added, will allow for more recipes");
-                }
+            if(formatted.length == 0) {
+                sendMessage(res, 400, "Could not find any other ingredients for any other recipes", [], "Get ingredients that, when added, will allow for more recipes");
+            }
 
-                formatted.sort(function(a, b) {
-                    return parseInt(a['ingredients_needed_count']) - parseInt(b['ingredients_needed_count']);
-                });
+            formatted.sort(function(a, b) {
+                return parseInt(a['ingredients_needed_count']) - parseInt(b['ingredients_needed_count']);
+            });
 
-                sendMessage(res, 200, mssg, formatted.slice(0,100), "Get ingredients that, when added, will allow for more recipes");
+                sendMessage(res, 200, mssg, formatted.slice(0,25), "Get ingredients that, when added, will allow for more recipes");
             });
         }
     };
